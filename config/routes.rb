1d5e0207b136689e50fb0b4bddd97a2b0565914b
Demian-Wicky <demian.wicky@gmail.com>
2021-11-01 20:54:30 +0100

    controllers and views for users & gossips created + team + contact + welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root to: 'gossips#index'
+  get '/contact', to: 'static_pages#contact'
+  get '/team', to: 'static_pages#team'
+  get '/welcome/:first_name', to: 'greets#greet'
+
+
+  resources :gossips, only: [:index, :show]
+  resources :users, only: [:show]
+
+
+
+
 end
